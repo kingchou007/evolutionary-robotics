896b7087f12981f2a99e478bae10ef99b7fdb1a2
jli45@uvm.edu <jli45@uvm.edu>
2021-02-06 00:36:08 +0800

    Simulation Done!

--- simulate.py
+++ simulate.py
@@ -1,15 +1,12 @@
 import pybullet as p
 import time as t
 
-# This creates an object, physicsClient, which handles the physics,
-# and draws the results to a Graphical User Interface (GUI).
 physicsClient = p.connect(p.GUI)
 
 # simulated world
 for i in range(1000):
-    # increases the physics inside the world for a small amount
     p.stepSimulation()
-    t.sleep(0.01666666666)
+    t.sleep(1/60)
 
 p.disconnect()
 
