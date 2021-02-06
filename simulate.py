e87aa0bd5c238925492974e03e1c20cecddf1edd
jli45@uvm.edu <jli45@uvm.edu>
2021-02-07 00:25:47 +0800

    One Link finished.

--- simulate.py
+++ simulate.py
@@ -3,6 +3,9 @@ import time as t
 
 physicsClient = p.connect(p.GUI)
 
+# pybullet (remember, we shortened it to p at the top of this file) to read in the world called box.
+p.loadSDF("box.sdf")
+
 # simulated world
 for i in range(1000):
     p.stepSimulation()
