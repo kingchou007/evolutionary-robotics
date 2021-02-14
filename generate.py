7d13ced5358954d957dab591200f833c7a1b2a4d
kane <jli45@uvm.edu>
2021-02-15 00:16:29 +0800

    Part_D Step 33 finished.

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
+# box
 pyrosim.Start_SDF("box.sdf")
 
 # size
@@ -7,13 +8,26 @@ length = 1
 width = 1
 height = 1
 
+
 # position
 x = 0
 y = 0
 z = 0.5
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x+0.5, y, z+1], size=[length, width, height])
+# tower
+for i in range(10):
+    for x in range(6):
+        for y in range(5):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+            y = y + 1
+        pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+        x = x + 1
+    # pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    z = z + 1
+    length = length * 0.9
+    width = width * 0.9
+    height = height * 0.9
 
 # Finish
 pyrosim.End()
+
