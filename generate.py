513217738f223c76de7b5915c685b87d2acfff41
kane <jli45@uvm.edu>
2021-02-14 21:23:28 +0800

    Two Links.

--- generate.py
+++ generate.py
@@ -6,8 +6,8 @@ x = 1
 y = 1
 z = 1
 
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0], size=[x, y, z])
-pyrosim.Send_Cube(name="Box2", pos=[0.5, 0, 1], size=[x, y, z])
+pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[x, y, z])
+pyrosim.Send_Cube(name="Box2", pos=[0.5, 0, 1.5], size=[x, y, z])
 
 # Finish
 pyrosim.End()
