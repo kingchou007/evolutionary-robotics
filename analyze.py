4b575dede3acf8eb0bf24c55c6ab3788184c4c4b
kane <jli45@uvm.edu>
2021-02-22 02:15:53 +0800

    Multiple sensors done.

--- analyze.py
+++ analyze.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot
-import numpy as np
+import numpy
 import os.path
 
-backLegSensorValues = np.load(os.path.join('data', 'backLegSensorValues.npy'))
-# print(backLegSensorValues)
-matplotlib.pyplot.show(backLegSensorValues)
+backLegSensorValues = numpy.load(os.path.join('data', 'backLegSensorValues.npy'))
+frontLegSensorValues = numpy.load(os.path.join('data', 'frontLegSensorValues.npy'))
+
+matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.show()
