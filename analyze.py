c43e4aa4d1d31800ef2f70ab4152be1fed3b91ff
kane <jli45@uvm.edu>
2021-02-22 02:40:17 +0800

    Prettifying the visualization (HW-3 all done).

--- analyze.py
+++ analyze.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot
 import numpy
 import os.path
 
+# load back leg data
 backLegSensorValues = numpy.load(os.path.join('data', 'backLegSensorValues.npy'))
+# load front leg data
 frontLegSensorValues = numpy.load(os.path.join('data', 'frontLegSensorValues.npy'))
 
-matplotlib.pyplot.plot(backLegSensorValues)
-matplotlib.pyplot.plot(frontLegSensorValues)
+# Visualization
+matplotlib.pyplot.plot(backLegSensorValues, label='back leg', linewidth=3)
+matplotlib.pyplot.plot(frontLegSensorValues, label='front leg')
+matplotlib.pyplot.title('Visualization')
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
+
