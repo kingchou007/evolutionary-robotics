2a8f6f91a970fc535ba2806fec49d02d4ce601d0
kane <jli45@uvm.edu>
2021-02-22 01:21:17 +0800

    Storing sensor values.

--- simulate.py
+++ simulate.py
@@ -1,7 +1,9 @@
 import pybullet as p
 import time as t
 import pybullet_data
+import numpy
 from pyrosim import pyrosim
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -20,12 +22,19 @@ p.loadSDF("world.sdf")
 # pyrosim setup
 pyrosim.Prepare_To_Simulate("body.urdf")
 
+# numpy vector
+backLegSensorValues = numpy.zeros(10000)
+
+
 # simulated world
 for i in range(1000):
     p.stepSimulation()
+    # Adding a touch sensor to the back leg
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     # print sensor values
     print(backLegTouch)
     t.sleep(1 / 60)
 
 p.disconnect()
+
+# print(backLegSensorValues)
