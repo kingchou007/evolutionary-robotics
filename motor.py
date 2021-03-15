6e6f52345417ef8e9748710d54304b9f2b459ed1
kane <jli45@uvm.edu>
2021-03-16 00:41:34 +0800

    Cleaning up - neurons finished.

--- motor.py
+++ motor.py
@@ -1,29 +1,11 @@
-import numpy
 import constants as c
 import pyrosim.pyrosim as pyrosim
 import pybullet as p
-import os.path
 
 
 class MOTOR:
     def __init__(self, jointName):
-        self.frequency = c.frontLegFrequency
-        self.offset = c.frontLegPhaseOffset
-        self.amplitude = c.frontLegAmplitude
         self.jointName = jointName
-        self.motorValues = numpy.zeros(c.timeSteps)
-        self.Prepare_To_Act(self.jointName)
-
-    def Prepare_To_Act(self, jointName):
-        if jointName == 'Torso_BackLeg':
-            self.amplitude = c.backLegAmplitude
-            self.frequency = c.backLegFrequency
-            self.offset = c.backLegPhaseOffset
-            self.motorValues = self.amplitude * numpy.sin(numpy.linspace(
-                -self.frequency * numpy.pi + self.offset, self.frequency * numpy.pi + self.offset, num=c.timeSteps))
-        else:
-            self.motorValues = self.amplitude * numpy.sin(numpy.linspace(
-                -self.frequency * numpy.pi + self.offset, self.frequency * numpy.pi + self.offset, num=c.timeSteps))
 
     def Set_Value(self, robot, desiredAngle):
         pyrosim.Set_Motor_For_Joint(
@@ -32,7 +14,3 @@ class MOTOR:
             controlMode=p.POSITION_CONTROL,
             targetPosition=desiredAngle,
             maxForce=c.motorForce)
-
-    def Save_Values(self):
-        numpy.save(os.path.join('data', str(self.jointName) + 'MotorValues.npy'), self.motorValues)
-
